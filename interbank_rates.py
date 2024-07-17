4da54430bee1552030e867fa9944ab387d18f104
jpkintu <kintujp@gmail.com>
2024-07-17 19:39:01 +0300

    update

--- interbank_rates.py
+++ interbank_rates.py
@@ -39,8 +39,8 @@ import statistics
 summary_stats=df.describe() 
 print(summary_stats)
 summary_stats.to_csv('assets/data/entire_df_summary_stats.txt',index =False)
-# mean calculatioon by month of each year
 
+# mean calculatioon by month of each year
 df.set_index('Date', inplace=True)
 monthly_mean =df.resample('ME').mean().plot(
     kind='line',
@@ -66,9 +66,65 @@ annual_mean.to_csv('assets/data/interbank_rates_annualmeans.csv')
 print(annual_mean)
 
 #breaking down my analysis to a period of 12 months (july 2023 - june 2024)
-# mean, median ,SD calculations by month 
+
+#1ST FILTER DATA FOR THE PERIOD JULY 2023-JUNE 2024, AND EXTRACT IN NEW CSV
 df=pd.read_csv("assets/data/Inter_Bank_Rates_Python_Cleaned.csv")
+start_date = '2023-07-01'
+end_date = '2024-06-30'
+
+filtered_df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+
+filtered_df.set_index('Date', inplace=True)
+
+filtered_df.to_csv('assets/data/inter_bank_rates_july23_june24.csv')
+
+# mean, median ,SD calculations by month 
+df_june24=pd.read_csv('assets/data/inter_bank_rates_july23_june24.csv')
+df_june24['Date'] = pd.to_datetime(df_june24['Date'])
+
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+import statistics
+
+#General Stats for 2023-2024
+fy_summary_stats=df_june24.describe() 
+print(fy_summary_stats)
+fy_summary_stats.to_csv('assets/data/fy_summary_stats.txt',index =False)
+
+# mean calculatioon by month 2023-2024
+df_june24.set_index('Date',inplace=True)
+fy_monthly_mean =df_june24.resample('ME').mean().plot(
+    kind='line',
+    xlabel="Months",
+    ylabel="Mean Inter Bank Rates",
+    title="Average Inter Bank Rates Spread over Months (JULY 2023 -JUNE-2024)"
+)
+plt.show();
+fy_monthly_mean.to_csv('assets/data/interbank_rates_means_by_month_2023_2024.csv')
+
+# median calculatioon by month 2023-2024
+df_june24.set_index('Date',inplace=True)
+fy_monthly_median =df_june24.resample('ME').median().plot(
+    kind='line',
+    xlabel="Months",
+    ylabel="Median Inter Bank Rates",
+    title="Median Inter Bank Rates Spread over Months (JULY 2023 -JUNE-2024)"
+)
+plt.show();
+fy_monthly_median.to_csv('assets/data/interbank_rates_median_by_month_2023_2024.csv')
+
+# SD calculatioon by month 2023-2024
+df_sd = df_june24.groupby(df_june24['Date'].dt.to_period('M')).agg({'Overnight_rate': 'std', 'Seven_day_rate': 'std', 'Overall_rate': 'std'}).plot(
+    kind='line',
+    xlabel="Months",
+    ylabel="SD Inter Bank Rates",
+    title="Standard Deviation Inter Bank Rates Spread over Months (JULY 2023 -JUNE-2024)"
+)
+plt.show();
+df_sd.to_csv('assets/data/interbank_rates_sd_by_month_2023_2024.csv')
 
 
-df.info()
-df.head()
+print(df_sd)
+df_june24.info()
+df_june24.head
